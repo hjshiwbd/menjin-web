26bacd61dd8239bd5d4029bc3304de65d0709511
hjshiwbd <34489659@qq.com>
2016-03-05 13:33:32 +0800

    事件接收启动/停止事件

---
+++ src/main/java/org/shinomin/menjin/listener/ContextStopListener.java
@@ -0,0 +1,32 @@
+package org.shinomin.menjin.listener;
+
+import org.shinomin.menjin.webservice.WsQuery;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * 服务启动时,自动执行
+ * 
+ * @author hjin
+ * 
+ */
+@Component
+public class ContextStopListener implements ApplicationListener<ContextStoppedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextStoppedEvent event) {
+		WsQuery.stopRecvRealEvent();
+		ApplicationContext app = event.getApplicationContext();
+		if (isCache(app)) {
+//			WsQuery.stopRecvRealEvent();
+		}
+	}
+
+	private boolean isCache(ApplicationContext app) {
+		return "Root WebApplicationContext".equals(app.getDisplayName())
+				|| "org.springframework.context.support.GenericApplicationContext".equals(app.getClass().getName());
+	}
+
+}
