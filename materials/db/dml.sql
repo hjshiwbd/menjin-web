8a1c767774c37eb2cb9c5a72ae0fc08fde14eb6c
xinqian <xinqian@juanpi.com>
2016-03-16 14:03:50 +0800

    去掉user insert的自增id部分

--- materials/db/dml.sql
+++ materials/db/dml.sql
@@ -1,1 +1,1 @@
-INSERT INTO [dbo].[mjsq_user] ([id], [username], [password]) VALUES ('1', 'admin', '53A7B2A913D54E24DF19E1B0EE5B16BF');
+INSERT INTO [dbo].[mjsq_user] ([username], [password]) VALUES (admin', '53A7B2A913D54E24DF19E1B0EE5B16BF');
